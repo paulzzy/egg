6aafd0b30303234214aed830c556039158d57177
Paul Zhang <paulz@ucla.edu>
2024-06-18 10:56:38 -0700

    Make `unionfind` API fallible with `Option`

--- src/unionfind.rs
+++ src/unionfind.rs
@@ -4,48 +4,50 @@ use std::fmt::Debug;
 #[derive(Debug, Clone, Default)]
 #[cfg_attr(feature = "serde-1", derive(serde::Serialize, serde::Deserialize))]
 pub struct UnionFind {
-    parents: Vec<Id>,
+    // TODO: Oof doubling memory usage and maybe destroying the cache hit rate
+    // by using Option<Id> instead of Id. Any way to use NonZeroU32 or similar?
+    parents: Vec<Option<Id>>,
 }
 
 impl UnionFind {
     pub fn make_set(&mut self) -> Id {
         let id = Id::from(self.parents.len());
-        self.parents.push(id);
+        self.parents.push(Some(id));
         id
     }
 
     pub fn size(&self) -> usize {
-        self.parents.len()
+        self.parents.iter().filter(|node| node.is_some()).count()
     }
 
-    fn parent(&self, query: Id) -> Id {
+    fn parent(&self, query: Id) -> Option<Id> {
         self.parents[usize::from(query)]
     }
 
-    fn parent_mut(&mut self, query: Id) -> &mut Id {
-        &mut self.parents[usize::from(query)]
+    fn parent_mut(&mut self, query: Id) -> Option<&mut Id> {
+        (&mut self.parents[usize::from(query)]).as_mut()
     }
 
-    pub fn find(&self, mut current: Id) -> Id {
-        while current != self.parent(current) {
-            current = self.parent(current)
+    pub fn find(&self, mut current: Id) -> Option<Id> {
+        while current != self.parent(current)? {
+            current = self.parent(current)?
         }
-        current
+        Some(current)
     }
 
-    pub fn find_mut(&mut self, mut current: Id) -> Id {
-        while current != self.parent(current) {
-            let grandparent = self.parent(self.parent(current));
-            *self.parent_mut(current) = grandparent;
+    pub fn find_mut(&mut self, mut current: Id) -> Option<Id> {
+        while current != self.parent(current)? {
+            let grandparent = self.parent(self.parent(current)?)?;
+            *self.parent_mut(current)? = grandparent;
             current = grandparent;
         }
-        current
+        Some(current)
     }
 
     /// Given two leader ids, unions the two eclasses making root1 the leader.
-    pub fn union(&mut self, root1: Id, root2: Id) -> Id {
-        *self.parent_mut(root2) = root1;
-        root1
+    pub fn union(&mut self, root1: Id, root2: Id) -> Option<Id> {
+        *self.parent_mut(root2)? = root1;
+        Some(root1)
     }
 
     /// TODO: Naive implementation, for a potentially more efficient one see
@@ -57,26 +59,23 @@ impl UnionFind {
     pub fn delete(&mut self, query: Id) {
         let parent = self.parent(query);
 
-        self.parents.remove(usize::from(query));
+        self.parents[usize::from(query)] = None;
 
         let mut new_root: Option<Id> = None;
         for idx in 0..self.parents.len() {
-            if parent == query {
+            if parent == Some(query) {
                 // Deleted a root node so choose a new root for the children, if any
-                if self.parents[idx] == query {
+                if self.parents[idx] == Some(query) {
                     if new_root.is_none() {
                         new_root = Some(Id::from(idx));
                     }
-                    self.parents[idx] = new_root.unwrap();
+                    self.parents[idx] = new_root;
                 }
             } else {
                 // Deleting a non-root node
-                if self.parents[idx] == query {
+                if self.parents[idx] == Some(query) {
                     self.parents[idx] = parent;
                 }
-            }
-            if self.parents[idx] > query {
-                self.parents[idx] = Id::from(usize::from(self.parents[idx]) - 1);
             }
         }
     }
@@ -86,8 +85,8 @@ impl UnionFind {
 mod tests {
     use super::*;
 
-    fn ids(us: impl IntoIterator<Item = usize>) -> Vec<Id> {
-        us.into_iter().map(|u| u.into()).collect()
+    fn ids(us: impl IntoIterator<Item = Option<usize>>) -> Vec<Option<Id>> {
+        us.into_iter().map(|u| u.map(|id| id.into())).collect()
     }
 
     #[test]
@@ -101,7 +100,7 @@ mod tests {
         }
 
         // test the initial condition of everyone in their own set
-        assert_eq!(uf.parents, ids(0..n));
+        assert_eq!(uf.parents, ids((0..n).map(Some)));
 
         // build up one set
         uf.union(id(0), id(1));
@@ -119,7 +118,18 @@ mod tests {
         }
 
         // indexes:         0, 1, 2, 3, 4, 5, 6, 7, 8, 9
-        let expected = vec![0, 0, 0, 0, 4, 5, 6, 6, 6, 6];
+        let expected = vec![
+            Some(0),
+            Some(0),
+            Some(0),
+            Some(0),
+            Some(4),
+            Some(5),
+            Some(6),
+            Some(6),
+            Some(6),
+            Some(6),
+        ];
         assert_eq!(uf.parents, ids(expected));
     }
 
@@ -138,15 +148,74 @@ mod tests {
         union_find.union(Id::from(7), Id::from(8));
         union_find.union(Id::from(8), Id::from(9));
 
-        assert_eq!(union_find.parents, ids(vec![0, 0, 0, 0, 4, 5, 6, 6, 7, 8]));
+        assert_eq!(
+            union_find.parents,
+            ids(vec![
+                Some(0),
+                Some(0),
+                Some(0),
+                Some(0),
+                Some(4),
+                Some(5),
+                Some(6),
+                Some(6),
+                Some(7),
+                Some(8)
+            ])
+        );
 
+        // Deletion leaves vacant nodes to avoid changing IDs (which correspond with indices)
+        // Since 0 is a root node, its children are assigned a new root (1)
         union_find.delete(Id::from(0));
-        assert_eq!(union_find.parents, ids(vec![0, 0, 0, 3, 4, 5, 5, 6, 7]));
+        assert_eq!(
+            union_find.parents,
+            ids(vec![
+                None,
+                Some(1),
+                Some(1),
+                Some(1),
+                Some(4),
+                Some(5),
+                Some(6),
+                Some(6),
+                Some(7),
+                Some(8)
+            ])
+        );
 
+        //
         union_find.delete(Id::from(4));
-        assert_eq!(union_find.parents, ids(vec![0, 0, 0, 3, 4, 4, 5, 6]));
+        assert_eq!(
+            union_find.parents,
+            ids(vec![
+                None,
+                Some(1),
+                Some(1),
+                Some(1),
+                None,
+                Some(5),
+                Some(6),
+                Some(6),
+                Some(7),
+                Some(8)
+            ])
+        );
 
-        union_find.delete(Id::from(4));
-        assert_eq!(union_find.parents, ids(vec![0, 0, 0, 3, 4, 4, 5]));
+        union_find.delete(Id::from(6));
+        assert_eq!(
+            union_find.parents,
+            ids(vec![
+                None,
+                Some(1),
+                Some(1),
+                Some(1),
+                None,
+                Some(5),
+                None,
+                Some(7),
+                Some(7),
+                Some(8)
+            ])
+        );
     }
 }
